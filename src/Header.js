860824c7dd1654966ff11e04ec8d638bcc1c522f
thales <thales.sousufpi@gmail.com>
2024-04-26 12:56:06 -0300

    refactor: header title

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Header() {
   return (
     <div className="title-container">
-      <h1>Horas trabalhadas da bbxinha!</h1>
+      <h1>Horas trabalhadas!</h1>
     </div>
   );
 }
